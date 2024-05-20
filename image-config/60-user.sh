2ffba843dee3ed3aef4c49704c55a452bff114d4
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2024-05-20 13:58:09 -0500

    add random sleep at startup

--- image-config/60-user.sh
+++ image-config/60-user.sh
@@ -13,6 +13,8 @@ chmod +x $CONNECT_DIR/sync_users.sh
 pushd $CONNECT_DIR
 echo "token=$API_TOKEN" > $CONNECT_DIR/token
 export API_TOKEN_FILE=$CONNECT_DIR/token
+# Random sleep to prevent thundering herd.
+sleep $((RANDOM%120))
 $CONNECT_DIR/sync_users.sh -u root.atlas-af -g root.atlas-af -e https://api.ci-connect.net:18080
 popd
 
