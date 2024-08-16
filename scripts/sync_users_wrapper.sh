080fd365ed8511937bf5f2f625dd98bcbdb76c9d
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2024-08-16 09:38:59 -0500

    update sync script with verbosity

--- scripts/sync_users_wrapper.sh
+++ scripts/sync_users_wrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sleep $((RANDOM%300))
+TIME=$((RANDOM%300))
+echo "Sleeping for $TIME seconds"
+sleep $TIME
 cd /usr/local/ciconnect 
 source /usr/local/ciconnect/config && ./sync_users.sh -u root.atlas-af -g root.atlas-af -e https://api.ci-connect.net:18080 >> /var/log/provisioner.log 2>&1
