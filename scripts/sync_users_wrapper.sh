0d2b6bf669a07d8a495c2077bd9ff4efe87f9e69
Fengping Hu <fengping@uchicago.edu>
2024-08-21 13:02:01 -0500

    yes to overwrite

--- scripts/sync_users_wrapper.sh
+++ scripts/sync_users_wrapper.sh
@@ -4,4 +4,4 @@ echo "Sleeping for $TIME seconds"
 sleep $TIME
 cd /usr/local/ciconnect 
 source /usr/local/ciconnect/config && ./sync_users.sh -u root.atlas-af -g root.atlas-af -e https://api.ci-connect.net:18080 >> /var/log/provisioner.log 2>&1
-[[ -d /passwd ]] && cp /etc/passwd /passwd
+[[ -d /passwd ]] && yes | cp /etc/passwd /passwd
